74cf67f0bd437ad5f62f18cdae701e218faa90bf
lium <stein1988>
2026-03-19 11:11:30 +0800

    refactor(base): 重构基础模块配置类路径并更新相关文档
    将基础模块的配置类从 `com.lonbon.cloud.base.config` 移动到 `com.lonbon.cloud.base.spring` 包下
    更新所有引用该路径的文档和配置文件

--- module-base/src/main/java/com/lonbon/cloud/base/spring/BaseAutoConfiguration.java
+++ module-base/src/main/java/com/lonbon/cloud/base/spring/BaseAutoConfiguration.java
@@ -1,4 +1,4 @@
-package com.lonbon.cloud.base.config;
+package com.lonbon.cloud.base.spring;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
